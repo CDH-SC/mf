d1f1cc883e871ea5085d77a2b0872a4c0457b3de
calebk <emperor.pickles@gmail.com>
2017-11-07 11:41:04 -0500

    Created models based on the dummy MF database

--- mf-api/models/entry.model.js
+++ mf-api/models/entry.model.js
@@ -2,3 +2,16 @@ var mongoose = require('mongoose')
 var mongoosePaginate = require('mongoose-paginate')
 
 //model aplication for entry scheme
+var Schema = mongoose.Schema;
+//Define schema
+var entrySchema = new Schema({
+  author:       String,
+  date:         String,
+  transcriber:  String,
+  textTranscribed: String,
+  textEditoral: String
+});
+
+//Export function to create "Entry" model class
+entrySchema.plugin(mongoosePaginate);
+module.exports = mongoose.model('Entry', entrySchema);
