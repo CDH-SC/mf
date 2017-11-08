191596c136068d9975e9e45c3b25f5a60b3257ae
alcamech <alcamech@gmail.com>
2017-11-08 15:24:43 -0500

    routing implemented
    Signed-off-by: alcamech <alcamech@gmail.com>

--- mf-angular/src/app/app.module.ts
+++ mf-angular/src/app/app.module.ts
@@ -2,21 +2,40 @@ import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutComponent } from './about/about.component';
+import { BrowseByHandComponent } from './browse-by-hand/browse-by-hand.component';
+import { BrowseByDateComponent } from './browse-by-date/browse-by-date.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'browse-by-date', component: BrowseByDateComponent},
+  { path: 'browse-by-hand', component: BrowseByHandComponent}
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    AboutComponent,
+    BrowseByHandComponent,
+    BrowseByDateComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [
 
