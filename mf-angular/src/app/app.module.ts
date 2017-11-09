b92fc4175dedf55fb8d572539d577b22f92ee24f
JMathis52 <jerrodmathis95@gmail.com>
2017-11-09 11:27:21 -0500

    Fixed minor bug in app.module.ts, line 21 (added a comma)

--- mf-angular/src/app/app.module.ts
+++ mf-angular/src/app/app.module.ts
@@ -18,8 +18,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'browse-by-date', component: BrowseByDateComponent},
-  { path: 'browse-by-hand', component: BrowseByHandComponent}
-  { path: 'about', component: AboutComponent },
+  { path: 'browse-by-hand', component: BrowseByHandComponent},
+  { path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
