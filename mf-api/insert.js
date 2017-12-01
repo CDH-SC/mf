9a23d75d3cdedadb06d461e832a8067f8ef1fb80
alcamech <alcamech@gmail.com>
2017-12-01 15:45:49 -0500

    Added notebook 18

--- mf-api/insert.js
+++ mf-api/insert.js
@@ -8,26 +8,26 @@ mongoose.connect('mongodb://127.0.0.1:27017/mf', { useMongoClient: true})
 .catch(()=> { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/mf`)})
 var conn = mongoose.connection;
 
-var base = 'Add_ms_46776_00';
+var base = 'Add_ms_46793_00';
 var page = [];
 for (i=1; i < 10; i++) {
   page.push(base+String(i)+".jpg");
 }
 
-base = 'Add_ms_46776_0';
+base = 'Add_ms_46793_0';
 for(i=10; i < 100; i++) {
   page.push(base+String(i)+".jpg");
 }
 
-base = 'Add_ms_46776_';
-for(i=100; i <= 191; i++) {
+base = 'Add_ms_46793_';
+for(i=100; i <= 488; i++) {
   page.push(base+String(i)+".jpg");
 }
 
 var newDiary = new Diary({
-  volume_id: 1,
-  date: "Oct. 1868-Jan. 1869",
-  volumeURL: "/images/notebook_one/",
+  volume_id: 18,
+  date: "1904",
+  volumeURL: "/images/notebook_18/",
   page: page,
 })
 
