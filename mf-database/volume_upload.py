87b2a79c324512abd404c592aae57353499e2cbc
alcamech <alcamech@gmail.com>
2018-01-29 14:31:59 -0500

    notebook_18 edited, initial volume_upload script

---
+++ mf-database/volume_upload.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# Volume Upload Script
+#
+# This is a volume upload script that will loop through the ../mf-archive/ directory
+# and its tei.xml files. It will automate the upload notebook process by pulling the
+# appropiate diary entries enclosed by <pb/> tags.
+
+import os
+import sys
+import re
+from bs4 import BeautifulSoup
+
+directory = "../mf-archive/"
+
+# loop through files in ../mf-archive directory
+
+for filename in os.listdir(directory):
+    if filename.endswith(".xml"):
+        file = open(os.path.join(directory, filename), "r")
+        content = file.read()
+        #print content
+        contentMatch = re.findall("<pb/>(.*?)(?=<pb/>)", content, re.DOTALL)
+        print "found "+str(len(contentMatch))+" entries for the following notebook: "+filename
