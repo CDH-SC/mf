ebfa0b30eedc1aacf9f25746402da90682d82344
Caleb Kitzmann <emperor.pickles@gmail.com>
2018-02-13 14:44:25 -0500

    added images for notebook 10 and reworked insert.js to create new document for notebook 10. Not correctly making page array

--- mf-api/insert.js
+++ mf-api/insert.js
@@ -8,27 +8,32 @@ mongoose.connect('mongodb://127.0.0.1:27017/mf', { useMongoClient: true})
 .catch(()=> { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/mf`)})
 var conn = mongoose.connection;
 
-var base = 'Add_ms_46793_00';
+var base = 'Add_ms_46785_00';
 var page = [];
 for (i=1; i < 10; i++) {
   page.push(base+String(i)+".jpg");
 }
 
-base = 'Add_ms_46793_0';
+base = 'Add_ms_46785_0';
 for(i=10; i < 100; i++) {
   page.push(base+String(i)+".jpg");
 }
 
-base = 'Add_ms_46793_';
-for(i=100; i <= 488; i++) {
+base = 'Add_ms_46785_';
+for(i=100; i <= 427; i++) {
   page.push(base+String(i)+".jpg");
 }
 
 var newDiary = new Diary({
-  volume_id: 18,
-  date: "1904",
-  volumeURL: "/images/notebook_18/",
-  page: page,
+  _id: 10,
+  date: "1896",
+  notebook_url: "/images/notebook_10/",
+  volume_num: "X",
+  ms_num: 46785,
+  page: [{
+    _id: false,
+    image: page,
+  }],
 })
 
 conn.collection('diaries').insert(newDiary);
