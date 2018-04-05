1119c1187c398a47768a4d3d6195422d44d021e3
Jerrod Mathis <jerrodmathis95@gmail.com>
2018-04-05 13:56:33 -0400

    Updated the about page, also fixed bug with jumpToFolio in top bar

--- mf-angular/src/app/about/about.component.ts
+++ mf-angular/src/app/about/about.component.ts
@@ -8,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    //For expanding the about sections
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for(i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      }
+      });
+    }
   }
 
 }
