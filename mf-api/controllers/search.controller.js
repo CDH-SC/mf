a430bf9a1c62087e593d35b4c13fbae6a76f93b4
Caleb Kitzmann <emperor.pickles@gmail.com>
2018-04-12 15:14:02 -0400

    more work on implementing search api

--- mf-api/controllers/search.controller.js
+++ mf-api/controllers/search.controller.js
@@ -4,11 +4,25 @@ _this = this
 
 exports.searchDiaries = async function(req, res){
   try {
-    var search = await SearchService.searchDiaries(search)
+    var searchResults = await SearchService.searchDiaries(search)
 
-    return res.status(200).json({status: 200, data: search, message: "Successfully searched diaries"});
+    return res.status(200).json({status: 200, data: searchResults, message: "Successfully searched diaries"});
 
   }catch(e){
     return res.status(400).json({status: 400, message: e.message});
+  }
+}
+
+//Async controller function to create search
+exports.createSearch = async function(req, res){
+
+  try {
+    //Calling service function with new object from request body
+    var createdSearch = await SearchService.createSearch(search)
+    return res.status(201).json({status: 201, data: createdSearch, message: "Successfully created search"})
+
+  }catch(e){
+    //Return error response with code and error message
+    return res.status(400).json({status: 400, message: "Search creation was unsuccessful"})
   }
 }
