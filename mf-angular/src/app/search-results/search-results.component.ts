6aad6fe747cbb4c56ea7d797f5e5243b63d1c148
emperorpickles <emperor.pickles@gmail.com>
2018-04-18 13:56:08 -0400

    formatting search results

--- mf-angular/src/app/search-results/search-results.component.ts
+++ mf-angular/src/app/search-results/search-results.component.ts
@@ -29,6 +29,21 @@ export class SearchResultsComponent implements OnInit {
       this.searchResults = res2["data"];
       console.log(this.searchResults);
     });
+    //For expanding the about sections
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for(i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
   }
 
 }
