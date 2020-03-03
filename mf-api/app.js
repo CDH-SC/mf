33d3feeb1dee15a455e8c2fc71e9b013fd0ae1bd
emperorpickles <emperor.pickles@gmail.com>
2020-03-03 14:33:00 -0500

    updating backend

--- mf-api/app.js
+++ mf-api/app.js
@@ -19,7 +19,7 @@ var app = express();
 
 var mongoose = require('mongoose')
 mongoose.Promise = bluebird
-mongoose.connect(process.env.DB_HOST, { useMongoClient: true})
+mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 .then(()=> { console.log(`Succesfully Connected to the Mongodb Database  at URL : mongodb://127.0.0.1:27017/mf`)})
 .catch(()=> { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/mf`)})
 
